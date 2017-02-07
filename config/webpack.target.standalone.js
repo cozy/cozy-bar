fa6fdb8b8301eb5dd403658fd5722368f4cec63e
m4dz <code@m4dz.net>
2017-02-07 11:53:20 +0100

    [fix] restore minifcation on build

--- config/webpack.target.standalone.js
+++ config/webpack.target.standalone.js
@@ -2,13 +2,19 @@
 
 const path = require('path')
 const merge = require('webpack-merge')
+const {filename, production} = require('./webpack.vars')
 const baseConfig = require('./webpack.config.base.js')
+const productionConfig = require('./webpack.config.prod')
 const inlineStylesConfig = require('./webpack.config.inline-styles.js')
-const {filename} = require('./webpack.vars')
 
-module.exports = merge(baseConfig, inlineStylesConfig, {
-  output: {
-    filename: filename('js', 'standalone'),
-    path: path.resolve(__dirname, '../dist')
+module.exports = merge(
+  baseConfig,
+  production ? productionConfig : {},
+  inlineStylesConfig,
+  {
+    output: {
+      filename: filename('js', 'standalone'),
+      path: path.resolve(__dirname, '../dist')
+    }
   }
-})
+)
