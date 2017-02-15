ac920e6557c07c9e9531fafed99334a6eada539d
Bruno Michel <brmichel@free.fr>
2017-02-15 23:27:38 +0100

    Send cookies with fetch (#11)

--- src/lib/stack.js
+++ src/lib/stack.js
@@ -3,11 +3,17 @@ import 'babel-polyfill'
 
 import { UnavailableStackException } from './exceptions'
 
+// the option credentials:include tells fetch to include the cookies in the
+// request even for cross-origin requests
+const fetchOptions = {
+  credentials: 'include'
+}
+
 module.exports = {
   get: {
     async apps () {
       try {
-        const response = await fetch('http://cozy.local:8080/apps/')
+        const response = await fetch('http://cozy.local:8080/apps/', fetchOptions)
         const data = await response.json()
         return data.data
       } catch (e) {
@@ -16,7 +22,7 @@ module.exports = {
     },
     async diskUsage () {
       try {
-        const response = await fetch('http://cozy.local:8080/settings/disk-usage')
+        const response = await fetch('http://cozy.local:8080/settings/disk-usage', fetchOptions)
         const data = await response.json()
         return data.data
       } catch (e) {
