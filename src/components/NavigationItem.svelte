a1d4e3d196ad05f2e8cab4ceda7d23955e1ab5bf
CPatchane <code@patchane.com>
2017-05-25 11:38:46 +0200

    fix: indentations :pencil2:

--- src/components/NavigationItem.svelte
+++ src/components/NavigationItem.svelte
@@ -1,11 +1,11 @@
 <li class='coz-nav-item'>
   {{#if item.component}}
-  <div role='menuitem' data-icon='{{dataIcon?dataIcon:""}}' aria-busy='{{isBusy}}'>
-    {{label}}
-    {{#if item.component === 'storage'}}
-    <Storage diskUsageFromStack='{{item.currentDiskUsage}}' diskQuotaFromStack='{{item.currentDiskQuota}}' />
-    {{/if}}
-  </div>
+    <div role='menuitem' data-icon='{{dataIcon?dataIcon:""}}' aria-busy='{{isBusy}}'>
+      {{label}}
+      {{#if item.component === 'storage'}}
+        <Storage diskUsageFromStack='{{item.currentDiskUsage}}' diskQuotaFromStack='{{item.currentDiskQuota}}' />
+      {{/if}}
+    </div>
   {{elseif item.inactive}}
     <div role='menuitem'>
       <p class='coz-bar-text-item coz-bar-text-item--inactive'>{{label}}</p>
@@ -13,14 +13,14 @@
   {{elseif item.href}}
     <a role='menuitem' href='{{item.href}}' target='{{item.external?"_blank":"_self"}}' data-icon='{{dataIcon?dataIcon:""}}'>
       {{#if fileIcon}}
-      <img src='{{fileIcon.src}}' alt='' width='64' height='64' class='{{fileIcon.class ? fileIcon.class : ''}}' />
+        <img src='{{fileIcon.src}}' alt='' width='64' height='64' class='{{fileIcon.class ? fileIcon.class : ''}}' />
       {{/if}}
       <p class='coz-label'>{{label}}</p>
     </a>
   {{elseif item.action}}
     <button role='menuitem' data-icon='{{dataIcon?dataIcon:""}}' on:click='proxy(item.action)'>
       {{#if fileIcon}}
-      <img src='{{fileIcon.src}}' alt='' width='64' height='64' class='{{fileIcon.class ? fileIcon.class : ''}}' />
+        <img src='{{fileIcon.src}}' alt='' width='64' height='64' class='{{fileIcon.class ? fileIcon.class : ''}}' />
       {{/if}}
       {{label}}
     </button>
