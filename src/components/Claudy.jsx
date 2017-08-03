f16821a2ee6d2c3b58bcad310cc77da196c6632b
CPatchane <code@patchane.com>
2017-08-03 23:41:45 +0200

    feat: better claudy active status handling :sparkles:

--- src/components/Claudy.jsx
+++ src/components/Claudy.jsx
@@ -5,7 +5,8 @@ class Claudy extends Component {
     super(props)
     this.store = context.store
     this.state = {
-      isLoading: false
+      isLoading: false,
+      isActive: false
     }
 
     this.toggle = this.toggle.bind(this)
@@ -20,30 +21,33 @@ class Claudy extends Component {
       this.setState({isLoading: true})
       this.store.getClaudyIntent({ exposeIntentFrameRemoval: true })
       .start(this.intentWrapperRef, () => {
-        this.setState({isLoading: false})
+        this.setState({isLoading: false, isActive: true})
         this.props.onToggle() // toggle claudy when the intent is loaded
       })
       .then(({ removeIntentFrame }) => { // exposeFrameRemoval intent event
         // remove the intent frame at the end of the menu closing transition
-        this.intentWrapperRef.addEventListener('transitionend', function closed (event) {
-          if (event.propertyName === 'transform') {
+        const closedListener = (e) => {
+          if (e.propertyName === 'transform') {
             removeIntentFrame()
-            this.removeEventListener('transitionend', closed, false)
+            this.setState({ isActive: false })
+            e.target.removeEventListener('transitionend', closedListener)
           }
-        })
+        }
+        this.intentWrapperRef.addEventListener('transitionend', closedListener, false)
         this.props.onToggle()
       })
     } else {
+      this.setState({ isActive: !this.state.isActive })
       this.props.onToggle()
     }
   }
 
   render () {
     const { opened } = this.props
-    const { isLoading } = this.state
+    const { isLoading, isActive } = this.state
     return (
       <div className={`coz-claudy ${opened ? 'coz-claudy--opened' : ''}`}>
-        <button className='coz-claudy-icon coz-bar-hide-sm' data-claudy-opened={opened} data-claudy-loading={isLoading} onClick={this.toggle} />
+        <button className='coz-claudy-icon coz-bar-hide-sm' data-claudy-opened={isActive} data-claudy-loading={isLoading} onClick={this.toggle} />
         <div
           class='coz-claudy-intent-wrapper'
           ref={(wrapper) => { this.intentWrapperRef = wrapper }}
