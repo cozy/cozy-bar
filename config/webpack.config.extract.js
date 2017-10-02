2acb897046155772f911883f83cc026476bb1edf
CPatchane <code@patchane.com>
2017-10-02 11:08:21 +0200

    chore: handle cozy-ui react components styles :wrench:

--- config/webpack.config.extract.js
+++ config/webpack.config.extract.js
@@ -1,22 +1,33 @@
 'use strict'
 
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
-
-const {filename} = require('./webpack.vars')
+const { filename } = require('./webpack.vars.js')
 
 module.exports = {
   resolve: {
     extensions: ['.styl']
   },
   module: {
-    loaders: [{
-      test: /\.styl$/,
-      loader: ExtractTextPlugin.extract([
-        'css-loader?importLoaders=1',
-        'postcss-loader',
-        'stylus-loader?paths=node_modules/cozy-ui/stylus/'
-      ])
-    }]
+    loaders: [
+      {
+        test: /\.styl$/,
+        exclude: /cozy-ui\/react/,
+        loader: ExtractTextPlugin.extract([
+          'css-loader?importLoaders=1',
+          'postcss-loader',
+          'stylus-loader?paths=node_modules/cozy-ui/stylus/'
+        ])
+      },
+      {
+        test: /\.styl$/,
+        include: /cozy-ui\/react/,
+        loader: ExtractTextPlugin.extract([
+          'css-loader?importLoaders=1&modules&localIdentName=[local]--[hash:base64:5]',
+          'postcss-loader',
+          'stylus-loader?paths=node_modules/cozy-ui/stylus/'
+        ])
+      }
+    ]
   },
   plugins: [
     new ExtractTextPlugin(filename('css'))
