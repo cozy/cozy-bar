16923634c5e0affe46e3267f8f75724d6e893225
Simon Constans <simonc@linux.com>
2017-12-12 10:35:37 +0100

    feat: Remove own app on mobile AppsList

--- src/lib/reducers/index.js
+++ src/lib/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux'
 import * as content from './content'
 import * as locale from './locale'
-import appsReducers, { getApps as getAppsWithoutProxy, fetchApps } from './apps'
+import appsReducer, * as apps from './apps'
 
 const proxy = (attr, method) => {
   return (state, ...args) => {
@@ -11,16 +11,19 @@ const proxy = (attr, method) => {
 
 const setContent = content.setContent
 const setLocale = locale.setLocale
-export { setContent, setLocale, fetchApps }
+const fetchApps = apps.fetchApps
+const setInfos = apps.setInfos
+export { setContent, setLocale, fetchApps, setInfos }
 
 export const getContent = proxy('content', content.getContent)
 export const getLocale = proxy('locale', locale.getLocale)
-export const getApps = proxy('apps', getAppsWithoutProxy)
+export const getApps = proxy('apps', apps.getApps)
+export const getAppsFiltered = proxy('apps', apps.getAppsFiltered)
 
 export const reducers = {
   content: content.reducer,
   locale: locale.reducer,
-  apps: appsReducers
+  apps: appsReducer
 }
 
 export default combineReducers(reducers)
