10e181d1e6fbf1932ea99d188d0a8382c7bfa698
Raphaël Rougeron <goldoraf@gmail.com>
2018-06-11 08:58:24 +0200

    feat: Added RTL swipe to close the drawer

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import Hammer from 'hammerjs'
 import Spinner from 'cozy-ui/react/Spinner'
 
 import { fetchApps, isAppListFetching } from '../lib/reducers'
@@ -11,6 +12,9 @@ class Drawer extends Component {
   constructor (props, context) {
     super(props)
     this.store = context.barStore
+    this.state = {
+      isScrolling: false
+    }
   }
 
   onDrawerClick = event => {
@@ -19,13 +23,25 @@ class Drawer extends Component {
     }
   }
 
+  onTransitionEnd = event => {
+    if (this.props.visible) {
+      this.attachGestures()
+      this.preventBackgroundScrolling()
+    } else {
+      this.detachGestures()
+      this.restoreBackgroundScrolling()
+    }
+    this.props.drawerListener()
+  }
+
   async componentWillMount () {
     await this.props.fetchAppsList()
     await this.store.fetchSettingsData()
   }
 
   componentDidMount () {
-    this.asideRef.addEventListener('transitionend', this.props.drawerListener)
+    this.turnTransitionsOn()
+    this.asideRef.addEventListener('transitionend', this.onTransitionEnd)
   }
 
   async componentWillReceiveProps (nextProps) {
@@ -33,6 +49,88 @@ class Drawer extends Component {
       await this.props.fetchAppsList()
       await this.store.fetchSettingsData()
     }
+  }
+
+  turnTransitionsOn () {
+    this.asideRef.classList.add('with-transition')
+  }
+
+  turnTransitionsOff () {
+    this.asideRef.classList.remove('with-transition')
+  }
+
+  preventBackgroundScrolling () {
+    document.body.style.overflow = 'hidden'
+  }
+
+  restoreBackgroundScrolling () {
+    document.body.style.overflow = 'auto'
+  }
+
+  detachGestures () {
+    this.gesturesHandler.destroy()
+  }
+
+  attachGestures () {
+    // IMPORTANT: on Chrome, the `overflow-y: scroll` property on .coz-drawer--apps prevented
+    // swipe events to be dispatched correctly ; the `touch-action: pan-y` fixes the problem
+    // see drawer.css
+    this.gesturesHandler = new Hammer.Manager(document.body, {
+      // we listen in all directions so that we can catch panup/pandown events and let the user scroll
+      recognizers: [[Hammer.Pan, { direction: Hammer.DIRECTION_ALL }]]
+    })
+
+    // to be completely accurate, `maximumGestureDelta` should be the difference between the right of the aside and the
+    // left of the page; but using the width is much easier to compute and accurate enough.
+    const maximumGestureDistance = this.asideRef.getBoundingClientRect().width
+    // between 0 and 1, how far down the gesture must be to be considered complete upon release
+    const minimumCloseDistance = 0.6
+    // a gesture faster than this will dismiss the menu, regardless of distance traveled
+    const minimumCloseVelocity = 0.6
+
+    let currentGestureProgress = null
+
+    this.gesturesHandler.on('panstart', event => {
+      if (event.additionalEvent === 'panup' || event.additionalEvent === 'pandown') {
+        this.setState({ isScrolling: true })
+      } else {
+        this.turnTransitionsOff()
+        currentGestureProgress = 0
+      }
+    })
+
+    this.gesturesHandler.on('pan', e => {
+      if (this.state.isScrolling) return
+      currentGestureProgress = -e.deltaX / maximumGestureDistance
+      this.applyTransformation(currentGestureProgress)
+    })
+
+    this.gesturesHandler.on('panend', e => {
+      if (this.state.isScrolling) {
+        this.setState({isScrolling: false})
+        return
+      }
+      // Dismiss the menu if the swipe pan was bigger than the treshold,
+      // or if it was a fast, downward gesture
+      let shouldDismiss =
+        -e.deltaX / maximumGestureDistance >= minimumCloseDistance ||
+        (-e.deltaX > 0 && e.velocity >= minimumCloseVelocity)
+
+      if (shouldDismiss) {
+        this.turnTransitionsOn()
+        this.props.onClose()
+        this.asideRef.style.transform = ''
+      } else {
+        this.turnTransitionsOn()
+        this.applyTransformation(0)
+      }
+    })
+  }
+
+  applyTransformation (progress) {
+    // constrain between 0 and 1.1 (go a bit further than 1 to be hidden completely)
+    progress = Math.min(1.1, Math.max(0, progress))
+    this.asideRef.style.transform = 'scaleX(' + (1 - progress) + ')'
   }
 
   render () {
