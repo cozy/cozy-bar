f7eba27bb3e0c9189e36adb8a30bd5d6eb2ea871
Éric D <15271+edas@users.noreply.github.com>
2019-02-16 21:42:15 +0100

    feat: Inject cozy-client - stage 3 - logout uses cozy-client

--- src/lib/stack-client.js
+++ src/lib/stack-client.js
@@ -1,10 +1,49 @@
 import internal from 'lib/stack-internal.js'
 
+import {
+  UnavailableStackException,
+  UnauthorizedStackException
+} from './exceptions'
+
 /**
  * Cozy client instance
  * @private
  */
 let cozyClient
+
+/**
+ * Get the stackClient from the cozy-client instance
+ *
+ * @private
+ * @function
+ * @returns {Object} cozy-stack-client instance
+ */
+const getStackClient = function() {
+  return cozyClient.getStackClient()
+}
+
+/**
+ * Logout and disconnect the user
+ * @function
+ * @TODO move this to cozy-stack-client
+ * @returns {Promise}
+ */
+const logout = function() {
+  return getStackClient().fetch('DELETE', '/auth/login').then(
+    resp => {
+      if (resp.status === 401) {
+        throw new UnauthorizedStackException()
+      } else if (resp.status === 204) {
+        window.location.reload()
+      }
+      return true
+    }
+  ).catch(
+    () => { 
+      throw new UnavailableStackException()
+    }
+  )
+}
 
 /**
  * Initializes the functions to call the cozy stack
@@ -26,4 +65,4 @@ const init = function(options) {
   return internal.init(legacyOptions)
 } 
 
-export default { ...internal, get: { ...internal.get}, init }
+export default { ...internal, get: { ...internal.get}, logout, init }
