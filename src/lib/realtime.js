3d801b537fec66775b15644f74278a080cd9a4dd
Patrick Browne <pt.browne@gmail.com>
2019-05-08 10:01:46 +0200

    refactor: Name handlers

--- src/lib/realtime.js
+++ src/lib/realtime.js
@@ -10,28 +10,30 @@ const APPS_DOCTYPE = 'io.cozy.apps'
  * @returns {Promise}
  */
 function initializeRealtime({ getApp, onCreate, onDelete, cozyClient }) {
+  const handleAppCreation = async app => {
+    // Fetch directly the app to get attributes `related` as well.
+    let fullApp
+    try {
+      fullApp = await getApp(app.slug)
+    } catch (error) {
+      throw new Error(`Cannot fetch app ${app.slug}: ${error.message}`)
+    }
+
+    if (typeof onCreate === 'function') {
+      onCreate(fullApp)
+    }
+  }
+
+  const handleAppRemoval = app => {
+    if (typeof onDelete === 'function') {
+      onDelete(app)
+    }
+  }
+
   try {
     const realtime = new CozyRealtime({ cozyClient })
-
-    realtime.subscribe('created', APPS_DOCTYPE, async app => {
-      // Fetch directly the app to get attributes `related` as well.
-      let fullApp
-      try {
-        fullApp = await getApp(app.slug)
-      } catch (error) {
-        throw new Error(`Cannot fetch app ${app.slug}: ${error.message}`)
-      }
-
-      if (typeof onCreate === 'function') {
-        onCreate(fullApp)
-      }
-    })
-
-    realtime.subscribe('deleted', APPS_DOCTYPE, app => {
-      if (typeof onDelete === 'function') {
-        onDelete(app)
-      }
-    })
+    realtime.subscribe('created', APPS_DOCTYPE, handleAppCreation)
+    realtime.subscribe('deleted', APPS_DOCTYPE, handleAppRemoval)
   } catch (error) {
     console.warn(`Cannot initialize realtime in Cozy-bar: ${error.message}`)
   }
