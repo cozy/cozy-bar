64c71e1fa246d8066ffbc9068121c60d9001c86d
Patrick Browne <pt.browne@gmail.com>
2019-06-04 21:21:24 +0200

    feat: Ability to do a light build

--- config/webpack.js
+++ config/webpack.js
@@ -18,6 +18,7 @@ module.exports = (env = {}) => {
   return merge(
     require('./webpack.config.base.js'),
     addAnalyzer ? require('./webpack.config.analyzer.js') : {},
+    env.light ? require('./webpack.target.light.js') : {},
     require('./webpack.config.jsx.js'),
     require(production ? './webpack.config.prod' : './webpack.config.dev'),
     cssConfig({ filename, mobile, production }),
---
+++ config/webpack.target.light.js
@@ -0,0 +1,10 @@
+module.exports = {
+  externals: {
+    'cozy-client': 'CozyClient',
+    'cozy-stack-client': 'CozyStackClient',
+    'cozy-ui': 'CozyUI',
+    lodash: '_',
+    'my-react-dom': 'ReactDOM',
+    'my-react': 'React'
+  }
+}
