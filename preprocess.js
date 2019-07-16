a345fc6d8fbd7fcd89c7f44d3cd9a536e04c5594
Patrick Browne <pt.browne@gmail.com>
2019-07-16 17:48:09 +0200

    feat: Make preprocess executable

--- preprocess.js
+++ preprocess.js
@@ -1,9 +1,14 @@
 const stylus = require('stylus')
 const cozyStylusPlugin = require('cozy-ui/stylus')
+const fs = require('fs')
 
 const renderStylus = function(css, filename) {
+  const nodeModulesPath = __dirname + '/node_modules'
+  console.log({ nodeModulesPath })
   try {
     return stylus(css)
+      .define('url', stylus.url({ paths: [nodeModulesPath] }))
+      .include(nodeModulesPath)
       .use(cozyStylusPlugin())
       .set('filename', filename)
       .render()
@@ -13,3 +18,8 @@ const renderStylus = function(css, filename) {
 }
 
 module.exports = renderStylus
+
+if (require.main === module) {
+  const css = fs.readFileSync(process.argv[2]).toString()
+  console.log(renderStylus(css, 'toto.css'))
+}
