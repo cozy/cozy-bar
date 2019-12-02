e6988bc35c78c7423f13fb87c79846e0aedd5e9b
Quentin Valmori <quentin.valmori@gmail.com>
2019-12-02 13:52:02 +0100

    fix: Read version from cozyclient instance

--- src/components/Settings/helper.js
+++ src/components/Settings/helper.js
@@ -1,13 +1,25 @@
-import CozyClient from 'cozy-client'
 import compare from 'semver-compare'
-
+import client from 'lib/stack-client'
 export const isFetchingQueries = requests => {
   return requests.some(request => request.fetchStatus === 'loading')
 }
-
+/**
+ *
+ * @param {cozyClient} forcedCozyClient only used to test purpose
+ *
+ * We can not read `version` from `import CozyClient from cozy-client`
+ * since in that case, we'll read version from the cozy-bar node modules
+ * and not from the app one.
+ *
+ * In order to avoid this issue, we get the cozyclient instance from
+ * lib/stack-client (cozyclient's instance passed by the app to the bar),
+ * then read the constructor and then read the version from it
+ */
 export const cozyClientCanCheckPremium = (forcedCozyClient = null) => {
   const cozyClientToUse =
-    forcedCozyClient !== null ? forcedCozyClient : CozyClient
+    forcedCozyClient !== null
+      ? forcedCozyClient
+      : client.getClient().constructor
   if (!cozyClientToUse.version) return false
   const result = compare(cozyClientToUse.version, '8.3.0')
   return result >= 0
