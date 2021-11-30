3b8f120bd36da7775790b7c2a0727a5734b8a1d2
Quentin Valmori <quentin.valmori@gmail.com>
2021-11-30 09:03:12 +0100

    fix: Change from piwik to matomo

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -12,7 +12,7 @@ module.exports = {
       __PIWIK_SITEID__: 8,
       __PIWIK_SITEID_MOBILE__: 12,
       __PIWIK_DIMENSION_ID_APP__: 1,
-      __PIWIK_TRACKER_URL__: JSON.stringify('https://piwik.cozycloud.cc')
+      __PIWIK_TRACKER_URL__: JSON.stringify('https://matomo.cozycloud.cc')
     })
   ]
 }
