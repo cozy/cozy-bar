d595088d3e4ade0bd2e4c82876d93059b110a699
Antonin Cezard <acezard@gmail.com>
2022-02-28 16:16:49 +0100

    feat: Allow cozy-flag to display home button
    Adds convenience for debugging purposes

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -1,4 +1,9 @@
+import set from 'lodash.set'
+
 import minilog from '@cozy/minilog'
+import flag from 'cozy-flags'
+
+set(window, `cozy.debug.flagship`, () => flag('flagship.debug', true))
 
 const logger = minilog('cozy-bar')
 minilog.suggest.deny('cozy-bar', 'info')
