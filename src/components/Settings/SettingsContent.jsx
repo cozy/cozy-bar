4244499d614bf742c78fcc2b58378923e1641c37
Théo Poizat <hello@zatteo.com>
2022-11-28 15:54:12 +0100

    feat: Update settings sections
    To fit with the structure of the new settings app.

--- src/components/Settings/SettingsContent.jsx
+++ src/components/Settings/SettingsContent.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import flag from 'cozy-flags'
 import { translate } from 'cozy-ui/react/I18n'
-import { Button, ButtonLink } from 'cozy-ui/react/Button'
+import { Button } from 'cozy-ui/react/Button'
 import { isMobileApp } from 'cozy-device-helper'
 import StorageData from 'components/Settings/StorageData'
 
@@ -14,38 +15,95 @@ const SettingsContent = ({
   onClaudy,
   isDrawer = false,
   isClaudyLoading,
-  toggleSupport,
   shoulDisplayViewOfferButton,
-  managerUrlPremiumLink,
-  viewOfferButtonText
+  managerUrlPremiumLink
 }) => (
   <div className="coz-nav-pop-content">
     {isDrawer && <hr />}
     {settingsAppURL && (
-      <ul className="coz-nav-group">
-        <li className="coz-nav-settings-item">
-          <a
-            role="menuitem"
-            href={`${settingsAppURL}#/profile`}
-            target="_self"
-            data-icon="icon-profile"
-            title={t('profile')}
-          >
-            <p className="coz-label">{t('profile')}</p>
-          </a>
-        </li>
-        <li className="coz-nav-settings-item">
-          <a
-            role="menuitem"
-            href={`${settingsAppURL}#/connectedDevices`}
-            target="_self"
-            data-icon="icon-connectedDevices"
-            title={t('connectedDevices')}
-          >
-            <p className="coz-label">{t('connectedDevices')}</p>
-          </a>
-        </li>
-      </ul>
+      <>
+        <ul className="coz-nav-group">
+          <li className="coz-nav-settings-item">
+            <a
+              role="menuitem"
+              href={`${settingsAppURL}#/profile`}
+              target="_self"
+              data-icon="icon-profile"
+              title={t('profile')}
+            >
+              <p className="coz-label">{t('profile')}</p>
+            </a>
+          </li>
+          {(!isDrawer || !isMobileApp()) && shoulDisplayViewOfferButton && (
+            <li className="coz-nav-settings-item">
+              <a
+                role="menuitem"
+                href={managerUrlPremiumLink}
+                target="_self"
+                data-icon="icon-cozy-circle"
+                title={t('plans')}
+              >
+                <p className="coz-label">{t('plans')}</p>
+              </a>
+              <span data-icon="icon-openwith"></span>
+            </li>
+          )}
+          {!isDrawer && storageData && (
+            <>
+              <li className="coz-nav-settings-item">
+                <a
+                  role="menuitem"
+                  data-icon="icon-graph-circle"
+                  target="_self"
+                  title={t('storage')}
+                  href={`${settingsAppURL}#/storage`}
+                  className="coz-nav-settings-item-storage-link"
+                >
+                  {t('storage')}
+                  <StorageData data={storageData} />
+                </a>
+              </li>
+            </>
+          )}
+        </ul>
+        <ul className="coz-nav-group">
+          {flag('settings.permissions-dashboard') && (
+            <li className="coz-nav-settings-item">
+              <a
+                role="menuitem"
+                href={`${settingsAppURL}#/permissions/slug`}
+                target="_self"
+                data-icon="icon-hand"
+                title={t('permissions')}
+              >
+                <p className="coz-label">{t('permissions')}</p>
+              </a>
+            </li>
+          )}
+          <li className="coz-nav-settings-item">
+            <a
+              role="menuitem"
+              href={`${settingsAppURL}#/connectedDevices`}
+              target="_self"
+              data-icon="icon-devices"
+              title={t('connectedDevices')}
+            >
+              <p className="coz-label">{t('connectedDevices')}</p>
+            </a>
+          </li>
+          <li className="coz-nav-settings-item">
+            <a
+              role="menuitem"
+              href={`${settingsAppURL}#/sessions`}
+              target="_self"
+              data-icon="icon-globe"
+              title={t('connections')}
+            >
+              <p className="coz-label">{t('connections')}</p>
+            </a>
+          </li>
+        </ul>
+      </>
     )}
     {isDrawer && onClaudy && !isMobileApp() && (
       <ul className="coz-nav-group">
@@ -63,54 +121,22 @@ const SettingsContent = ({
         </li>
       </ul>
     )}
-    {!isDrawer && storageData && (
-      <ul className="coz-nav-group">
+    <ul className="coz-nav-group">
+      {!isMobileApp() && (
         <li className="coz-nav-settings-item">
           <a
             role="menuitem"
-            data-icon="icon-storage"
-            target="_self"
-            title={t('storage')}
-            href={`${settingsAppURL}#/storage`}
-          >
-            {t('storage')}
-            <StorageData data={storageData} />
-          </a>
-        </li>
-      </ul>
-    )}
-    {(!isDrawer || !isMobileApp()) && shoulDisplayViewOfferButton && (
-      <ul className="coz-nav-group">
-        <li className="coz-nav-settings-item">
-          <ButtonLink
-            subtle
-            role="menuitem"
-            className="coz-nav-settings-item-btn"
-            icon="cloud-happy"
-            title={viewOfferButtonText}
-            label={viewOfferButtonText}
-            href={managerUrlPremiumLink}
-          />
-        </li>
-      </ul>
-    )}
-
-    {!isMobileApp() && (
-      <ul className="coz-nav-group">
-        <li className="coz-nav-settings-item">
-          <Button
-            type="button"
-            role="menuitem"
-            className="coz-nav-settings-item-btn"
-            onClick={toggleSupport}
-            icon="help"
+            href="https://support.cozy.io/"
+            target="_blank"
+            rel="noopener noreferrer"
+            data-icon="icon-help"
             title={t('help')}
-            label={t('help')}
-          />
+          >
+            <p className="coz-label">{t('help')}</p>
+          </a>
+          <span data-icon="icon-openwith"></span>
         </li>
-      </ul>
-    )}
-    <ul className="coz-nav-group">
+      )}
       <li className="coz-nav-settings-item">
         <button
           type="button"
@@ -138,8 +164,6 @@ SettingsContent.propTypes = {
   storageData: PropTypes.object,
   onClaudy: PropTypes.oneOfType([PropTypes.bool, PropTypes.func]),
   isDrawer: PropTypes.bool,
-  isClaudyLoading: PropTypes.bool,
-  toggleSupport: PropTypes.func.isRequired,
-  viewOfferButtonText: PropTypes.string
+  isClaudyLoading: PropTypes.bool
 }
 export default translate()(SettingsContent)
