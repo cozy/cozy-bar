5e0420c6a64e7c7e6b621a99c127c77d17acb796
cballevre <celestin.ballevre@protonmail.com>
2023-06-19 22:26:34 +0200

    refactor(Banner): Convert from class to functional component

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,41 +1,34 @@
-import React, { Component } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { ButtonLink } from 'cozy-ui/transpiled/react/deprecated/Button'
-import { translate } from 'cozy-ui/transpiled/react/providers/I18n'
+import { useI18n } from 'cozy-ui/transpiled/react/providers/I18n'
 
-class Banner extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { unmounted: true }
-    this.animate = this.animate.bind(this)
-  }
+const Banner = ({ code, links }) => {
+  const { t } = useI18n()
+  const [unmounted, setUmounted] = useState(true)
 
-  animate() {
+  const animate = useCallback(() => {
     // To animate we have to use a setTimeout to
     // force a CSS class update and trigger CSS animation
     return setTimeout(() => {
-      this.setState(() => ({ unmounted: false }))
+      setUmounted(false)
     }, 100)
-  }
+  }, [setUmounted])
 
-  componentDidMount() {
-    this.animate()
-  }
+  useEffect(() => {
+    animate()
+  }, [animate])
 
-  render() {
-    const { t, code, links } = this.props
-    const { unmounted } = this.state
-    return (
-      <div className={`coz-bar-banner${unmounted ? ' unmounted' : ''}`}>
-        <p>{t(`banner.${code}.description`)}</p>
-        <ButtonLink
-          className="coz-bar-banner-button"
-          size="tiny"
-          href={links}
-          label={t(`banner.${code}.CTA`)}
-        />
-      </div>
-    )
-  }
+  return (
+    <div className={`coz-bar-banner${unmounted ? ' unmounted' : ''}`}>
+      <p>{t(`banner.${code}.description`)}</p>
+      <ButtonLink
+        className="coz-bar-banner-button"
+        size="tiny"
+        href={links}
+        label={t(`banner.${code}.CTA`)}
+      />
+    </div>
+  )
 }
 
-export default translate()(Banner)
+export default Banner
