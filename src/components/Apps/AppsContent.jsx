e4f86eb45c1256b760af29ef10fe226faa7c68e3
cballevre <celestin.ballevre@protonmail.com>
2023-06-19 22:37:08 +0200

    refactor(AppsContent): Convert from class to functional component

--- src/components/Apps/AppsContent.jsx
+++ src/components/Apps/AppsContent.jsx
@@ -1,11 +1,11 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 
-import { translate } from 'cozy-ui/transpiled/react/I18n'
+import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 import Icon from 'cozy-ui/transpiled/react/Icon'
 import CloudIcon from 'cozy-ui/transpiled/react/Icons/Cloud'
-import withBreakpoints from 'cozy-ui/transpiled/react/helpers/withBreakpoints'
+import useBreakpoints from 'cozy-ui/transpiled/react/hooks/useBreakpoints'
 
 import { getApps, getHomeApp, isFetchingApps } from 'lib/reducers'
 import AppItem from 'components/Apps/AppItem'
@@ -13,66 +13,59 @@ import AppItemPlaceholder from 'components/Apps/AppItemPlaceholder'
 
 const sorter = fn => (itemA, itemB) => fn(itemA) > fn(itemB)
 
-export class AppsContent extends Component {
-  constructor(props, context) {
-    super(props, context)
-    this.translateApp = translateApp(this.props.t)
+export const AppsContent = ({
+  apps,
+  homeApp,
+  isFetchingApps,
+  onAppSwitch,
+  isInvertedTheme
+}) => {
+  const { t } = useI18n()
+  const { isMobile } = useBreakpoints()
+  const translateAppWithLocales = translateApp(t)
+
+  const isHomeApp = homeApp && homeApp.isCurrentApp
+  const homeSlug = homeApp && homeApp.slug
+
+  if (!isFetchingApps && (!apps || !apps.length)) {
+    return <p className="coz-nav--error coz-nav-group">{t('no_apps')}</p>
   }
 
-  render() {
-    const {
-      t,
-      apps,
-      breakpoints,
-      homeApp,
-      isFetchingApps,
-      onAppSwitch,
-      isInvertedTheme
-    } = this.props
-    const { isMobile } = breakpoints
-    const isHomeApp = homeApp && homeApp.isCurrentApp
-    const homeSlug = homeApp && homeApp.slug
-
-    if (!isFetchingApps && (!apps || !apps.length)) {
-      return <p className="coz-nav--error coz-nav-group">{t('no_apps')}</p>
-    }
-
-    return (
-      <div className="coz-nav-pop-content">
-        <ul className="coz-nav-group">
-          {isMobile && homeApp && (
-            <AppItem
-              app={homeApp}
-              useHomeIcon
-              onAppSwitch={onAppSwitch}
-              isInvertedTheme={isInvertedTheme}
-            />
-          )}
-          {isFetchingApps
-            ? new Array(3)
-                .fill({})
-                .map((nothing, index) => <AppItemPlaceholder key={index} />)
-            : apps
-                .filter(app => app.slug !== homeSlug)
-                .sort(sorter(this.translateApp))
-                .map((app, index) => (
-                  <AppItem
-                    app={app}
-                    key={index}
-                    onAppSwitch={onAppSwitch}
-                    isInvertedTheme={isInvertedTheme}
-                  />
-                ))}
-        </ul>
-        {homeApp && !isMobile && !isHomeApp && (
-          <a role="menuitem" href={homeApp.href} className="coz-apps-home-btn">
-            <Icon icon={CloudIcon} />
-            {t('menu.home')}
-          </a>
+  return (
+    <div className="coz-nav-pop-content">
+      <ul className="coz-nav-group">
+        {isMobile && homeApp && (
+          <AppItem
+            app={homeApp}
+            useHomeIcon
+            onAppSwitch={onAppSwitch}
+            isInvertedTheme={isInvertedTheme}
+          />
         )}
-      </div>
-    )
-  }
+        {isFetchingApps
+          ? new Array(3)
+              .fill({})
+              .map((nothing, index) => <AppItemPlaceholder key={index} />)
+          : apps
+              .filter(app => app.slug !== homeSlug)
+              .sort(sorter(translateAppWithLocales))
+              .map((app, index) => (
+                <AppItem
+                  app={app}
+                  key={index}
+                  onAppSwitch={onAppSwitch}
+                  isInvertedTheme={isInvertedTheme}
+                />
+              ))}
+      </ul>
+      {homeApp && !isMobile && !isHomeApp && (
+        <a role="menuitem" href={homeApp.href} className="coz-apps-home-btn">
+          <Icon icon={CloudIcon} />
+          {t('menu.home')}
+        </a>
+      )}
+    </div>
+  )
 }
 
 AppsContent.propTypes = {
@@ -100,6 +93,4 @@ const mapStateToProps = state => ({
   isFetchingApps: isFetchingApps(state)
 })
 
-export default connect(mapStateToProps)(
-  translate()(withBreakpoints()(AppsContent))
-)
+export default connect(mapStateToProps)(AppsContent)
