d9a2f4917be6d93141cb730590d9a73a6f79e9c8
cballevre <celestin.ballevre@protonmail.com>
2023-06-19 22:16:17 +0200

    refactor(AppNavButton): Convert from class to functional

--- src/components/Apps/AppNavButtons.jsx
+++ src/components/Apps/AppNavButtons.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 
@@ -11,76 +11,72 @@ import { ButtonCozyHome } from 'components/Apps/ButtonCozyHome'
 import { getHomeApp } from 'lib/reducers'
 import { isFetchingApps } from 'lib/reducers'
 
-class AppNavButton extends Component {
-  render() {
-    const {
-      homeApp,
-      handleClick,
-      appName,
-      appNamePrefix,
-      appSlug,
-      iconPath,
-      isInvertedTheme,
-      isFetchingApps,
-      isPublic,
-      opened,
-      t
-    } = this.props
+const AppNavButton = ({
+  homeApp,
+  handleClick,
+  appName,
+  appNamePrefix,
+  appSlug,
+  iconPath,
+  isInvertedTheme,
+  isFetchingApps,
+  isPublic,
+  opened,
+  t
+}) => {
+  const isHomeApp = homeApp && homeApp.isCurrentApp
 
-    const isHomeApp = homeApp && homeApp.isCurrentApp
-
-    if (!isPublic && isFetchingApps) {
-      return (
-        <div className="coz-nav-apps-btns --loading">
-          <div className="coz-nav-apps-btns-home coz-loading-placeholder" />
-          <div className="coz-nav-apps-btns-main coz-loading-placeholder" />
-        </div>
-      )
-    }
-
-    const displayName =
-      !isHomeApp && appNamePrefix
-        ? [
-            t(`${appSlug}.name_prefix`, {
-              _: appNamePrefix
-            }),
-            t(`${appSlug}.name`, {
-              _: appName
-            })
-          ].join(' ')
-        : t(`${appSlug}.name`, { _: appName })
-
-    const homeHref = !isPublic && homeApp && homeApp.href
-
+  if (!isPublic && isFetchingApps) {
     return (
-      <div className={`coz-nav-apps-btns${isHomeApp ? ' --currentHome' : ''}`}>
-        <ButtonCozyHome homeHref={homeHref} isInvertedTheme={isInvertedTheme} />
-
-        {!isHomeApp && <span className="coz-nav-apps-btns-sep" />}
-
-        <button
-          type="button"
-          onClick={isPublic ? null : handleClick}
-          className="coz-nav-apps-btns-main"
-          aria-controls="coz-nav-pop--apps"
-          data-tutorial="apps"
-          disabled={isPublic}
-        >
-          {!isHomeApp && (
-            <img className="coz-bar-hide-sm" src={iconPath} width="28" alt="" />
-          )}
-          <span className="coz-nav-app-name">{displayName}</span>
-          {!isPublic && (
-            <Icon
-              icon={opened ? TopIcon : BottomIcon}
-              color="#95999d"
-              size="12"
-            />
-          )}
-        </button>
+      <div className="coz-nav-apps-btns --loading">
+        <div className="coz-nav-apps-btns-home coz-loading-placeholder" />
+        <div className="coz-nav-apps-btns-main coz-loading-placeholder" />
       </div>
     )
   }
+
+  const displayName =
+    !isHomeApp && appNamePrefix
+      ? [
+          t(`${appSlug}.name_prefix`, {
+            _: appNamePrefix
+          }),
+          t(`${appSlug}.name`, {
+            _: appName
+          })
+        ].join(' ')
+      : t(`${appSlug}.name`, { _: appName })
+
+  const homeHref = !isPublic && homeApp && homeApp.href
+
+  return (
+    <div className={`coz-nav-apps-btns${isHomeApp ? ' --currentHome' : ''}`}>
+      <ButtonCozyHome homeHref={homeHref} isInvertedTheme={isInvertedTheme} />
+
+      {!isHomeApp && <span className="coz-nav-apps-btns-sep" />}
+
+      <button
+        type="button"
+        onClick={isPublic ? null : handleClick}
+        className="coz-nav-apps-btns-main"
+        aria-controls="coz-nav-pop--apps"
+        data-tutorial="apps"
+        disabled={isPublic}
+      >
+        {!isHomeApp && (
+          <img className="coz-bar-hide-sm" src={iconPath} width="28" alt="" />
+        )}
+        <span className="coz-nav-app-name">{displayName}</span>
+        {!isPublic && (
+          <Icon
+            icon={opened ? TopIcon : BottomIcon}
+            color="#95999d"
+            size="12"
+          />
+        )}
+      </button>
+    </div>
+  )
 }
 
 AppNavButton.propTypes = {
