71a45f8e73601861f4e312e27bb9b5cc1b149f17
cballevre <celestin.ballevre@protonmail.com>
2023-08-11 09:50:37 +0200

    fix: Use as with io.cozy.settings improve cache between apps

--- src/queries/index.js
+++ src/queries/index.js
@@ -1,16 +1,16 @@
 import { Q } from 'cozy-client'
 
 export const instanceReq = {
-  query: () => Q('io.cozy.settings').getById('instance'),
-  as: 'instanceQuery'
+  query: () => Q('io.cozy.settings').getById('io.cozy.settings.instance'),
+  as: 'io.cozy.settings/io.cozy.settings.instance'
 }
 
 export const contextReq = {
   query: () => Q('io.cozy.settings').getById('context'),
-  as: 'contextQuery'
+  as: 'io.cozy.settings/context'
 }
 
 export const diskUsageReq = {
   query: () => Q('io.cozy.settings').getById('disk-usage'),
-  as: 'diskUsageQuery'
+  as: 'io.cozy.settings/disk-usage'
 }
