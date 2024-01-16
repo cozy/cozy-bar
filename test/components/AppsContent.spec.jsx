0e47791e5fa747487b2ea7dbd306d30aadcf3ba7
cballevre <celestin.ballevre@protonmail.com>
2024-01-16 15:33:57 +0100

    refactor(AppsContent): Convert from class to functional component

--- test/components/AppsContent.spec.jsx
+++ test/components/AppsContent.spec.jsx
@@ -1,19 +1,68 @@
 import React from 'react'
+import { render, screen } from '@testing-library/react'
+
+import { createMockClient } from 'cozy-client'
+import useBreakpoints from 'cozy-ui/transpiled/react/providers/Breakpoints'
+
 import { AppsContent } from 'components/Apps/AppsContent'
-import { shallow } from 'enzyme'
-import { tMock } from '../jestLib/I18n'
+import { BarLike } from '../lib/BarLike'
+import stack from 'lib/stack'
+
+jest.mock('cozy-ui/transpiled/react/providers/Breakpoints', () => ({
+  ...jest.requireActual('cozy-ui/transpiled/react/providers/Breakpoints'),
+  __esModule: true,
+  default: jest.fn()
+}))
+
+jest.mock('components/Apps/AppItemPlaceholder', () => {
+  function AppItemPlaceholder() {
+    return <div>AppItemPlaceholder</div>
+  }
+  AppItemPlaceholder.displayName = 'AppItemPlaceholder'
+  return AppItemPlaceholder
+})
 
 describe('Apps Content', () => {
-  it('should render no App', () => {
-    const root = shallow(
-      <AppsContent
-        t={tMock}
-        breakpoints={{ isMobile: true }}
-        isFetchingApps={false}
-      />
+  const setup = ({
+    isFetchingApps = false,
+    isMobile = false,
+    apps,
+    homeApp
+  } = {}) => {
+    useBreakpoints.mockReturnValue({ isMobile })
+
+    const mockClient = createMockClient({
+      clientOptions: {
+        uri: 'http://cozy.tools:8080'
+      }
+    })
+
+    stack.init({
+      cozyClient: mockClient
+    })
+
+    return render(
+      <BarLike client={mockClient}>
+        <AppsContent
+          isFetchingApps={isFetchingApps}
+          apps={apps}
+          homeApp={homeApp}
+        />
+      </BarLike>
     )
-    expect(root.getElement()).toMatchSnapshot()
+  }
+
+  afterEach(() => {
+    jest.resetAllMocks()
   })
+
+  it('should render no App', () => {
+    setup({ isMobile: true })
+    expect(
+      screen.getByText('No applications found on the Cozy.')
+    ).toBeInTheDocument()
+  })
+
   it('should render display Drive', () => {
     const apps = [
       {
@@ -21,15 +70,8 @@ describe('Apps Content', () => {
         name: 'Drive'
       }
     ]
-    const root = shallow(
-      <AppsContent
-        t={tMock}
-        apps={apps}
-        breakpoints={{ isMobile: true }}
-        isFetchingApps={false}
-      />
-    )
-    expect(root.getElement()).toMatchSnapshot()
+    setup({ apps, isMobile: true })
+    expect(screen.getByText('Drive')).toBeInTheDocument()
   })
 
   it('should render Loader when fetching', () => {
@@ -39,15 +81,8 @@ describe('Apps Content', () => {
         name: 'Drive'
       }
     ]
-    const root = shallow(
-      <AppsContent
-        t={tMock}
-        apps={apps}
-        breakpoints={{ isMobile: true }}
-        isFetchingApps={true}
-      />
-    )
-    expect(root.getElement()).toMatchSnapshot()
+    setup({ apps, isMobile: true, isFetchingApps: true })
+    expect(screen.getAllByText('AppItemPlaceholder')).toHaveLength(3)
   })
 
   it('should display home in the app items lists, if we are on mobile', () => {
@@ -57,18 +92,10 @@ describe('Apps Content', () => {
         name: 'Home'
       }
     ]
-    const root = shallow(
-      <AppsContent
-        t={tMock}
-        apps={apps}
-        homeApp={{
-          slug: 'cozy-home'
-        }}
-        breakpoints={{ isMobile: true }}
-        isFetchingApps={false}
-      />
-    )
-    expect(root.getElement()).toMatchSnapshot()
+
+    setup({ apps, isMobile: true })
+
+    expect(screen.getByText('Home')).toBeInTheDocument()
   })
 
   it('should not display home within the app items list, but just a link', () => {
@@ -78,18 +105,15 @@ describe('Apps Content', () => {
         name: 'Home'
       }
     ]
-    const root = shallow(
-      <AppsContent
-        t={tMock}
-        apps={apps}
-        homeApp={{
-          slug: 'cozy-home'
-        }}
-        breakpoints={{ isMobile: false }}
-        isFetchingApps={false}
-      />
-    )
-    expect(root.getElement()).toMatchSnapshot()
+
+    setup({
+      apps,
+      homeApp: {
+        slug: 'cozy-home'
+      }
+    })
+
+    expect(screen.queryByText('Home')).toBeNull()
   })
 
   it('should not display home if not on mobile and home is the currentApp', () => {
@@ -99,18 +123,15 @@ describe('Apps Content', () => {
         name: 'Home'
       }
     ]
-    const root = shallow(
-      <AppsContent
-        t={tMock}
-        apps={apps}
-        homeApp={{
-          slug: 'cozy-home',
-          isCurrentApp: true
-        }}
-        breakpoints={{ isMobile: false }}
-        isFetchingApps={false}
-      />
-    )
-    expect(root.getElement()).toMatchSnapshot()
+
+    setup({
+      apps,
+      homeApp: {
+        slug: 'cozy-home',
+        isCurrentApp: true
+      }
+    })
+
+    expect(screen.queryByText('Home')).toBeNull()
   })
 })
