ec56c47405e408e7511344c5fa61120d6e4f6336
cballevre <celestin.ballevre@protonmail.com>
2024-01-22 09:04:39 +0100

    chore: Build everything with Babel under cozy-bar

--- babel.config.js
+++ babel.config.js
@@ -2,33 +2,28 @@ const pkg = require('./package.json')
 
 module.exports = {
   presets: ['cozy-app'],
-  env: {
-    transpilation: {
-      presets: [
-        [
-          'cozy-app',
-          { presetEnv: { modules: false }, transformRuntime: { helpers: true } }
-        ]
-      ],
-      plugins: [
-        [
-          'module-resolver',
-          {
-            root: ['./src'],
-            alias: {
-              'cozy-ui/react': 'cozy-ui/transpiled/react'
-            }
-          }
-        ],
-        [
-          'transform-define',
-          {
-            __VERSION__: JSON.stringify(pkg.version),
-            'typeof window': 'object'
-          }
-        ]
-      ],
-      ignore: ['**/*.spec.jsx', '**/*.spec.js']
-    }
-  }
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src']
+      }
+    ],
+    [
+      'transform-define',
+      {
+        __VERSION__: JSON.stringify(pkg.version),
+        'typeof window': 'object'
+      }
+    ],
+    [
+      'css-modules-transform',
+      {
+        extensions: ['.styl'],
+        preprocessCss: './preprocess',
+        extractCss: './dist/stylesheet.css',
+        generateScopedName: '[local]'
+      }
+    ]
+  ]
 }
