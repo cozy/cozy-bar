884bcf1ee0e1bdb487b4e351562dd6552a404a9e
cballevre <celestin.ballevre@protonmail.com>
2024-02-06 18:22:58 +0100

    refactor(Settings): Use query to get instance info

--- src/components/Settings/helper.js
+++ src/components/Settings/helper.js
@@ -1,7 +1,6 @@
 import { compareClientVersion } from 'lib/stack-client'
-export const isFetchingQueries = requests => {
-  return requests.some(request => request.fetchStatus === 'loading')
-}
+export const isFetchingQueries = requests =>
+  requests.some(request => request.fetchStatus === 'loading')
 
 export const cozyClientCanCheckPremium = () => {
   return compareClientVersion('8.3.0')
