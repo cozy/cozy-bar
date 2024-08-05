d8a5fcd1fe2ce795106d0aefc892a8ebd4245c9a
JF-Cozy <julien.ficheux@cozycloud.cc>
2024-08-05 16:24:26 +0200

    feat: Replace Settings menu by ActionsMenu / ActionsItems

--- src/components/Settings/StorageData.jsx
+++ src/components/Settings/StorageData.jsx
@@ -2,40 +2,36 @@ import React from 'react'
 
 import { useInstanceInfo } from 'cozy-client'
 import { makeDiskInfos } from 'cozy-client/dist/models/instance'
+import { LinearProgress } from 'cozy-ui/transpiled/react/Progress'
 
 import useI18n from 'components/useI18n'
 
 const StorageData = () => {
   const { t } = useI18n()
-  const { isLoaded, ...instanceInfo } = useInstanceInfo()
+  const { isLoaded, diskUsage } = useInstanceInfo()
 
   if (!isLoaded) return null
 
   // we used this default quota set in getStorageData, we just reuse it here
   const storageData = makeDiskInfos(
-    instanceInfo.diskUsage.data.attributes.used,
-    instanceInfo.diskUsage.data.attributes.quota ||
-      Math.max(
-        10 ** 12,
-        10 * parseInt(instanceInfo.diskUsage.data.attributes.used, 10)
-      )
+    diskUsage.data.attributes.used,
+    diskUsage.data.attributes.quota ||
+      Math.max(10 ** 12, 10 * parseInt(diskUsage.data.attributes.used, 10))
   )
 
   return (
-    <div className="coz-nav-storage">
-      <p className="coz-nav-storage-text">
-        {t('storage_phrase', {
-          diskUsage: storageData.humanDiskUsage,
-          diskQuota: storageData.humanDiskQuota
-        })}
-      </p>
-      <progress
-        className="cozy-nav-storage-bar"
-        value={storageData.humanDiskUsage}
-        max={storageData.humanDiskQuota}
-        min="0"
+    <>
+      {t('storage_phrase', {
+        diskUsage: storageData.humanDiskUsage,
+        diskQuota: storageData.humanDiskQuota
+      })}
+      <LinearProgress
+        style={{ width: '85%' }}
+        className="u-mt-half"
+        variant="determinate"
+        value={parseInt(storageData.percentUsage)}
       />
-    </div>
+    </>
   )
 }
 
--- src/components/Settings/index.jsx
+++ src/components/Settings/index.jsx
@@ -5,7 +5,6 @@ import { useInstanceInfo } from 'cozy-client'
 import Button from 'cozy-ui/transpiled/react/Buttons'
 import Icon from 'cozy-ui/transpiled/react/Icon'
 import GearIcon from 'cozy-ui/transpiled/react/Icons/Gear'
-import ClickAwayListener from 'cozy-ui/transpiled/react/ClickAwayListener'
 
 import SettingsContent from 'components/Settings/SettingsContent'
 import useI18n from 'components/useI18n'
@@ -17,28 +16,26 @@ const Settings = ({ onLogOut }) => {
   const { isLoaded } = useInstanceInfo()
 
   return (
-    <ClickAwayListener onClickAway={() => setOpen(false)}>
-      <div className="coz-nav coz-nav-settings" ref={rootRef}>
-        <Button
-          variant="text"
-          aria-controls="coz-nav-pop--settings"
-          color="default"
-          busy={!isLoaded}
-          startIcon={<Icon icon={GearIcon} />}
-          label={t('menu.settings')}
-          onClick={() => setOpen(v => !v)}
+    <>
+      <Button
+        ref={rootRef}
+        variant="text"
+        aria-controls="simple-menu"
+        aria-haspopup="true"
+        color="default"
+        busy={!isLoaded}
+        startIcon={<Icon icon={GearIcon} />}
+        label={t('menu.settings')}
+        onClick={() => setOpen(v => !v)}
+      />
+      {isOpen && (
+        <SettingsContent
+          anchorRef={rootRef}
+          onLogOut={onLogOut}
+          onClose={() => setOpen(false)}
         />
-        {isLoaded && (
-          <div
-            className="coz-nav-pop coz-nav-pop--settings"
-            id="coz-nav-pop--settings"
-            aria-hidden={!isOpen}
-          >
-            <SettingsContent onLogOut={onLogOut} />
-          </div>
-        )}
-      </div>
-    </ClickAwayListener>
+      )}
+    </>
   )
 }
 
