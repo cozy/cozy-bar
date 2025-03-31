cd2f293aade961f33a631a65481be82e994fca75
Théo Poizat <hello@zatteo.com>
2025-03-31 15:14:48 +0200

    feat: Use a link to allow right click in help link

--- src/components/HelpLink/index.jsx
+++ src/components/HelpLink/index.jsx
@@ -3,13 +3,15 @@ import IconButton from 'cozy-ui/transpiled/react/IconButton'
 import Icon from 'cozy-ui/transpiled/react/Icon'
 import HelpOutlinedIcon from 'cozy-ui/transpiled/react/Icons/HelpOutlined'
 
-const openHelpLink = () => {
-  window.open('https://support.cozy.io/', '_blank', 'noopener, noreferrer')
-}
-
 const HelpLink = () => {
   return (
-    <IconButton onClick={openHelpLink} className="u-p-half">
+    <IconButton
+      component="a"
+      href="https://support.cozy.io/"
+      target="_blank"
+      rel="noopener, noreferrer"
+      className="u-p-half"
+    >
       <Icon icon={HelpOutlinedIcon} size="18" />
     </IconButton>
   )
