4bef3108aa82ee97c518277c3e7f60d8ea46ffc3
Théo Poizat <hello@zatteo.com>
2025-04-25 13:45:27 +0200

    feat: Use avatar from cozy-stack

--- src/components/UserMenu/components/AvatarMyself.jsx
+++ src/components/UserMenu/components/AvatarMyself.jsx
@@ -1,35 +1,21 @@
 import React from 'react'
 
-import { useQuery } from 'cozy-client'
-import { getInitials } from 'cozy-client/dist/models/contact'
+import { useClient } from 'cozy-client'
 import Avatar from 'cozy-ui/transpiled/react/Avatar'
-import Skeleton from 'cozy-ui/transpiled/react/Skeleton'
-
-import { buildMyselfQuery } from 'lib/queries'
 
 const AvatarMyself = ({ className, size = 'm' }) => {
-  const myselfQuery = buildMyselfQuery()
+  const client = useClient()
+  const rootURL = client.getStackClient().uri
 
-  const { fetchStatus, data } = useQuery(
-    myselfQuery.definition,
-    myselfQuery.options
+  return (
+    <Avatar className={className} size={size}>
+      <img
+        width="100%"
+        height="100%"
+        src={`${rootURL}/public/avatar?fallback=initials`}
+      />
+    </Avatar>
   )
-
-  if (fetchStatus !== 'loaded') {
-    return <Skeleton variant="circle" width={32} height={32} />
-  }
-
-  const me = data[0]
-
-  if (me) {
-    const initials = getInitials(me)
-
-    return (
-      <Avatar className={className} size={size}>
-        {initials}
-      </Avatar>
-    )
-  }
 }
 
 export default AvatarMyself
