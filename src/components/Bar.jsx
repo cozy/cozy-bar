79b46bd977f598937cd6b06e7af2bf0da2da5fcd
Théo Poizat <hello@zatteo.com>
2025-05-14 11:56:44 +0200

    feat: Remove isTwakeTheme
    Because isTwakeTheme was false in tests and UI has changed a lot when
    in Twake theme, I had to :
    - remove 2 tests about fetching apps when the drawer was opened that
    are not relevant anymore
    - update a test that show that we can override bar component. I had
    to use BarLeft instead of BarRight because there are no more default
    value for BarRight

--- src/components/Bar.jsx
+++ src/components/Bar.jsx
@@ -1,20 +1,15 @@
-import React, { useEffect, useState, useCallback } from 'react'
+import React, { useEffect, useCallback } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 
 import Grid from 'cozy-ui/transpiled/react/Grid'
 import Divider from 'cozy-ui/transpiled/react/Divider'
 import { useBreakpoints } from 'cozy-ui/transpiled/react/providers/Breakpoints'
-import Icon from 'cozy-ui/transpiled/react/Icon'
 import AppTitle from 'cozy-ui/transpiled/react/AppTitle'
 import { isFlagshipApp } from 'cozy-device-helper'
 import flag from 'cozy-flags'
-import { isTwakeTheme } from 'cozy-ui/transpiled/react/helpers/isTwakeTheme'
 import { ButtonCozyHome } from 'components/Apps/ButtonCozyHome'
 import Banner from 'components/Banner'
-import Drawer from 'components/Drawer'
-import Settings from 'components/Settings'
-import Apps from 'components/Apps'
 import AppsMenu from 'components/AppsMenu'
 import UserMenu from 'components/UserMenu'
 import HelpLink from 'components/HelpLink'
@@ -25,30 +20,16 @@ import {
   fetchContext,
   fetchSettingsData
 } from 'lib/reducers'
-import useI18n from 'components/useI18n'
 import { useClient } from 'cozy-client'
 import { AssistantDesktop } from 'cozy-search'
 import SearchButton from './Search/SearchButton'
 import cx from 'classnames'
-
-/* Generated with node_modules/.bin/svgr src/assets/sprites/icon-apps.svg */
-function SvgIconApps(props) {
-  return (
-    <svg width={16} height={16} {...props}>
-      <path
-        d="M0 0h4v4H0V0zm0 6h4v4H0V6zm0 6h4v4H0v-4zM6 0h4v4H6V0zm0 6h4v4H6V6zm0 6h4v4H6v-4zm6-12h4v4h-4V0zm0 6h4v4h-4V6zm0 6h4v4h-4v-4z"
-        fillRule="evenodd"
-      />
-    </svg>
-  )
-}
 
 export const Bar = ({
   fetchContext,
   fetchSettingsData,
   fetchApps,
   isPublic,
-  onDrawer,
   barLeft,
   barRight,
   barCenter,
@@ -57,20 +38,13 @@ export const Bar = ({
   userActionRequired,
   searchOptions,
   isInvertedTheme,
-  appName,
-  appNamePrefix,
   appSlug,
-  iconPath,
   hasFetchedApps,
   homeApp,
   componentsProps
 }) => {
   const client = useClient()
-  const { t } = useI18n()
   const { isMobile } = useBreakpoints()
-  const [drawerVisible, setDrawerVisible] = useState(false)
-  const showDrawerAndBurger = !isPublic && isMobile && !isTwakeTheme()
-  const showSettings = !isPublic && !isMobile
 
   const fetchInitialData = useCallback(() => {
     if (!isPublic) {
@@ -95,28 +69,8 @@ export const Bar = ({
     }
   }, [client, fetchInitialData])
 
-  useEffect(() => {
-    if (!hasFetchedApps && drawerVisible) {
-      fetchApps()
-    }
-  }, [drawerVisible, fetchApps, hasFetchedApps])
-
-  const toggleDrawer = () => {
-    onDrawer(!drawerVisible)
-    setDrawerVisible(!drawerVisible)
-  }
-
   const renderCenter = () => {
-    return isTwakeTheme() ? null : (
-      <Apps
-        appName={appName}
-        appNamePrefix={appNamePrefix}
-        appSlug={appSlug}
-        iconPath={iconPath}
-        isPublic={isPublic}
-        isInvertedTheme={isInvertedTheme}
-      />
-    )
+    return null
   }
 
   const renderLeft = () => {
@@ -124,44 +78,25 @@ export const Bar = ({
       return <ButtonCozyHome isInvertedTheme={isInvertedTheme} />
     }
 
-    if (isTwakeTheme()) {
-      const homeHref = !isPublic && homeApp && homeApp.href
+    const homeHref = !isPublic && homeApp && homeApp.href
 
-      if (isMobile) {
-        return <ButtonCozyHome homeHref={homeHref} />
-      }
-
-      const isHome = appSlug === 'home'
-
-      return (
-        <Grid container alignItems="center" className="u-w-auto">
-          {!isHome && (
-            <>
-              <ButtonCozyHome homeHref={homeHref} />
-              <Divider orientation="vertical" className="u-mr-half" flexItem />
-            </>
-          )}
-          <AppTitle slug={appSlug} />
-        </Grid>
-      )
+    if (isMobile) {
+      return <ButtonCozyHome homeHref={homeHref} />
     }
 
-    // data-tutorial attribute allows to be targeted in an application tutorial
-    return showDrawerAndBurger ? (
-      <button
-        type="button"
-        className="coz-bar-btn coz-bar-burger"
-        onClick={toggleDrawer}
-        data-tutorial="apps-mobile"
-      >
-        <Icon icon={SvgIconApps} width={16} height={16} color="currentColor" />
-        <span className="u-visuallyhidden">{t('drawer')}</span>
-      </button>
-    ) : null
-  }
+    const isHome = appSlug === 'home'
 
-  const renderRight = () => {
-    return showSettings ? <Settings onLogOut={onLogOut} /> : null
+    return (
+      <Grid container alignItems="center" className="u-w-auto">
+        {!isHome && (
+          <>
+            <ButtonCozyHome homeHref={homeHref} />
+            <Divider orientation="vertical" className="u-mr-half" flexItem />
+          </>
+        )}
+        <AppTitle slug={appSlug} />
+      </Grid>
+    )
   }
 
   const renderTwakeRight = () => {
@@ -206,23 +141,8 @@ export const Bar = ({
         {barCenter || renderCenter()}
         <div className="u-flex-grow">{barSearch || renderSearch()}</div>
         {searchOptions.enabled && isMobile ? <SearchButton /> : null}
-        {isTwakeTheme() ? (
-          <>
-            {barRight}
-            {!isPublic && renderTwakeRight()}
-          </>
-        ) : (
-          barRight || renderRight()
-        )}
-        {showDrawerAndBurger ? (
-          <Drawer
-            visible={drawerVisible}
-            onClose={toggleDrawer}
-            drawerListener={() => onDrawer(drawerVisible)}
-            onLogOut={onLogOut}
-            isInvertedTheme={isInvertedTheme}
-          />
-        ) : null}
+        {barRight}
+        {!isPublic && renderTwakeRight()}
       </div>
       {userActionRequired && <Banner {...userActionRequired} />}
     </div>
@@ -230,14 +150,10 @@ export const Bar = ({
 }
 
 Bar.propTypes = {
-  appName: PropTypes.string,
-  appNamePrefix: PropTypes.string,
   appSlug: PropTypes.string,
-  iconPath: PropTypes.string,
   isPublic: PropTypes.bool,
   isInvertedTheme: PropTypes.bool,
   onLogOut: PropTypes.func,
-  onDrawer: PropTypes.func,
   userActionRequired: PropTypes.object,
   componentsProps: PropTypes.shape({
     Wrapper: PropTypes.shape({
