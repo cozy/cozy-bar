d48bb1139824308f1f71a46491ffab035a8b3a44
Théo Poizat <hello@zatteo.com>
2025-09-23 15:47:28 +0200

    feat: Add ru and vi locales

--- src/components/useI18n.jsx
+++ src/components/useI18n.jsx
@@ -3,10 +3,15 @@ import { createUseI18n } from 'cozy-ui/transpiled/react/providers/I18n'
 import enLocale from 'locales/en.json'
 import frLocale from 'locales/fr.json'
 import esLocale from 'locales/es.json'
+import ruLocale from 'locales/ru.json'
+import viLocale from 'locales/vi.json'
+
 const locales = {
   en: enLocale,
   fr: frLocale,
-  es: esLocale
+  es: esLocale,
+  ru: ruLocale,
+  vi: viLocale
 }
 
 const useI18n = createUseI18n(locales)
