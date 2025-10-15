df55eed280cd3f0cddacf9bc7195a462c94ec75f
Théo Poizat <hello@zatteo.com>
2025-10-15 09:00:17 +0200

    feat: Use entrypoints helpers from cozy-client
    We also select only entrypoints we want.

--- src/components/AppsMenu/helper.js
+++ src/components/AppsMenu/helper.js
@@ -1,10 +1,25 @@
-export const buildEntrypoints = apps => {
-  return apps.flatMap(app => {
-    if (!app.entrypoints) return []
+import { models } from 'cozy-client'
 
-    return app.entrypoints.map(entrypoint => ({
-      ...entrypoint,
-      slug: app.slug
-    }))
-  })
+const {
+  applications: { selectEntrypoints, filterEntrypoints }
+} = models
+
+// We get only the entrypoints we want:
+// - Drive onlyoffice
+export const getEntrypoints = apps => {
+  const driveApp = apps.find(app => app.slug === 'drive') || {}
+
+  const driveEntrypoints = driveApp.entrypoints || []
+
+  const selectedEntrypoints = selectEntrypoints(driveEntrypoints, [
+    'new-file-type-text',
+    'new-file-type-sheet',
+    'new-file-type-slide'
+  ])
+  const filteredEntrypoints = filterEntrypoints(selectedEntrypoints)
+
+  return filteredEntrypoints.map(entrypoint => ({
+    ...entrypoint,
+    slug: driveApp.slug
+  }))
 }
